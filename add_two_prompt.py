34ac3c65bd29f9e4b5bac740c14eb45ff7749043
Gitzendanner <magitz@ufl.edu>
2020-05-05 15:33:50 -0400

    Add numpy,scipy,pandas

---
+++ add_two_prompt.py
@@ -0,0 +1,8 @@
+def add_two(a,b):
+    c=a+b
+    return c
+
+num1=float(input("Please type one number:"))
+num2=float(input("Please type another number:"))
+
+print(num1, "+", num2,"=", add_two(num1,num2))
