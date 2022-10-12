689dd416cbc9a6c28540c65b9318e8ce6b70d133
Matt Gitzendanner <magitz@ufl.edu>
2022-10-12 10:12:15 -0400

    Update path to data file for search5.py

--- search5.py
+++ search5.py
@@ -1,4 +1,8 @@
-fhand = open('mbox-short.txt')
+#!/usr/bin/env python
+
+# Edited to refernce file in data folder
+
+fhand = open('data/mbox-short.txt')
 for line in fhand:
     line = line.rstrip()
     if not line.startswith('From '): continue
